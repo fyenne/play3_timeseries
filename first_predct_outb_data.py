2a48429d86585b32e3798376912991e7c854579b
fyenne <fyenne@hotmail.com>
2021-09-01 17:42:09 +0800

    a

--- first_predct_outb_data.py
+++ first_predct_outb_data.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import os
 df = pd.DataFrame()
+os.listdir('./outb_data')
+pd.read_excel('./outb_data/2019.xlsx')
 for i in os.listdir('./outb_data'):
-    df = df.append(pd.read_csv('./outb_data/' + i, encoding='utf_8_sig'))
+    df = pd.concat([df, pd.read_excel('./outb_data/' + i)],\
+        axis = 0)
+ 
 df.to_csv('./outb_fas.csv', index = False,  encoding='utf_8_sig')
+
+import easyocr
