98a775b772aca2a7dd0546c7737de849a65be585
fyenne <fyenne@hotmail.com>
2021-09-01 14:56:21 +0800

    11

--- first_predct_outb_data.py
+++ first_predct_outb_data.py
@@ -1,3 +1,6 @@
 import pandas as pd
-df = pd.read_clipboard()
-df
+import os
+df = pd.DataFrame()
+for i in os.listdir('./outb_data'):
+    df = df.append(pd.read_csv('./outb_data/' + i, encoding='utf_8_sig'))
+df.to_csv('./outb_fas.csv', index = False,  encoding='utf_8_sig')
